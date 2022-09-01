0095b677a9ff8bf89f33b2208c49f63a48f449da
jomi707 <dol7966@gmail.com>
2022-09-01 16:10:46 +0900

    1.3: useTabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import { useInput } from "./API/useInput";
+import React, { useState } from "react";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "content of the Section 2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
 
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <h1>hello</h1>
-      <input placeholder="name" {...name} />
-      {/* {...name} === value={name.value} */}
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
