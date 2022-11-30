49907a8100bff732cacf32bbdd8d225ad80edbe6
jomi707 <dol7966@gmail.com>
2022-11-30 20:42:48 +0900

    2.6

--- nomadcoder-next/pages/index.tsx
+++ nomadcoder-next/pages/index.tsx
@@ -1,36 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/seo";
-import { useEffect, useState } from "react";
-import { resourceLimits } from "worker_threads";
-const API_KEY = "8ee0a535e520c10664813954d0e39ff4";
 
-export default function Home() {
-  const [movies, setMovies] = useState<any[]>();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }: any) {
+  const router = useRouter();
+  const onClick = (id: string) => {
+    router.push(`/movies/${id}`);
+  };
   return (
     <>
       <Seo title="Home"></Seo>
-      <div>
-        {movies ? (
-          movies?.map((movie: any) => (
-            <div key={movie.id}>{movie.original_title}</div>
+      <div className="container">
+        {results ? (
+          results.map((movie: any) => (
+            <div
+              onClick={() => onClick(movie.id)}
+              className="movie"
+              key={movie.id}
+            >
+              <Image
+                width={100}
+                height={150}
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt="postImage"
+              />
+              <Link href={`/api/movies/${movie.id}`}>
+                {movie.original_title}
+              </Link>
+            </div>
           ))
         ) : (
-          <h4>Loading...</h4>
+          <h4 className="loading">Loading...</h4>
         )}
-        {/* {!movies && <h4>Loading</h4>}
-        {movies?.map((movie: any) => (
-          <div key={movie.id}>{movie.original_title}</div>
-        ))} */}
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 20px;
+            gap: 20px;
+          }
+          .movie .img {
+            max-width: 100%;
+            border-radius: 12px;
+            transition: transform 0.2s ease-in-out;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          }
+          .movie:hover .img {
+            transform: scale(1.05) translateY(-10px);
+          }
+          .movie .link {
+            font-size: 18px;
+            text-align: center;
+          }
+        `}</style>
       </div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
